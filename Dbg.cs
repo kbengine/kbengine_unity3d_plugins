14ee787209d3fe12f342f73eb3b257e4bb607d1f
kbengine <liquidx@163.com>
2014-12-06 10:17:38 +0800

    up

--- Dbg.cs
+++ Dbg.cs
@@ -1,27 +1,34 @@
 ﻿using UnityEngine;
 using KBEngine;
+using System;
 using System.Collections;
+
 namespace KBEngine
 {
-public class Dbg {
-	public static void INFO_MSG(object s)
-	{
-		Debug.Log(s);
+	public class Dbg {
+		public static string getHead()
+		{
+			return "[" + DateTime.Now.ToString() + "]";
+		}
+
+		public static void INFO_MSG(object s)
+		{
+			Debug.Log(getHead() + s);
+		}
+		
+		public static void DEBUG_MSG(object s)
+		{
+			Debug.Log(getHead() + s);
+		}
+		
+		public static void WARNING_MSG(object s)
+		{
+			Debug.LogWarning(getHead() + s);
+		}
+		
+		public static void ERROR_MSG(object s)
+		{
+			Debug.LogError(getHead() + s);
+		}
 	}
-	
-	public static void DEBUG_MSG(object s)
-	{
-		Debug.Log(s);
-	}
-	
-	public static void WARNING_MSG(object s)
-	{
-		Debug.LogWarning(s);
-	}
-	
-	public static void ERROR_MSG(object s)
-	{
-		Debug.LogError(s);
-	}
-}
 }
