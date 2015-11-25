49656ee288f4936eae25eafb5cbd196262d3e8d4
kbengine <liquidx@163.com>
2015-11-25 15:04:09 +0800

    up

--- KBEngine.cs
+++ KBEngine.cs
@@ -86,7 +86,7 @@
 		
 		// 服务端与客户端的版本号以及协议MD5
 		public string serverVersion = "";
-		public string clientVersion = "0.6.21";
+		public string clientVersion = "0.7.0";
 		public string serverScriptVersion = "";
 		public string clientScriptVersion = "0.1.0";
 		public string serverProtocolMD5 = "";
