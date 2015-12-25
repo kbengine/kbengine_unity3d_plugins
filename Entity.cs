34df371bb3904e56abce91bdd0ef96158182dcaf
kbengine <liquidx@163.com>
2015-12-25 22:10:32 +0800

    up

--- Entity.cs
+++ Entity.cs
@@ -128,7 +128,7 @@
 				{
 					if((prop.isBase() && inited) || inWorld)
 					{
-						Dbg.DEBUG_MSG(className + "::notifyPropertysSetMethods(" + prop.name + ")");  
+						//Dbg.DEBUG_MSG(className + "::notifyPropertysSetMethods(" + prop.name + ")");  
 						setmethod.Invoke(this, new object[]{oldval});
 					}
 				}
