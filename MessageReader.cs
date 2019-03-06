efc5201a42896d1f01a1e61b2f86f92e91758c07
kebiao <380000937@qq.com>
2019-03-06 15:10:23 +0800

    up

--- MessageReader.cs
+++ MessageReader.cs
@@ -155,7 +155,6 @@
 						totallen += expectSize;
 						length -= expectSize;
 
-						int frpos = stream.rpos + (int)expectSize;
 						Message msg = Message.clientMessages[msgid];
 						
 #if UNITY_EDITOR
@@ -163,16 +162,11 @@
 #endif
 
 						msg.handleMessage(stream);
+
 #if UNITY_EDITOR
 						Dbg.profileEnd(msg.name);
 #endif
 						
-						if (expectSize > 0 && frpos != stream.rpos)
-						{
-								Dbg.WARNING_MSG("MessageReader::process(" + msg.name + "): rpos(" + stream.rpos + ") invalid, expect="
-										+ frpos + ", msgID=" + msgid + ", msglen=" + expectSize + ".");
-						}
-
 						stream.clear();
 						
 						state = READ_STATE.READ_STATE_MSGID;
